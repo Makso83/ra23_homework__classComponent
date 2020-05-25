1b74dd8f808b937cd845f57057b1995ef847a85e
Brani <makkolot@gmail.com>
2020-05-25 11:32:41 +0300

    homework classComponent v0.1.0

---
+++ src/ShopItemClass/ShopItemClass.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ShopItemClass extends React.Component {
+  render() {
+    return (
+      <div class="main-content">
+        <h2>{this.props.item.brand}</h2>
+        <h1>{this.props.item.title}</h1>
+        <h3>{this.props.item.description}</h3>
+        <div class="description">{this.props.item.descriptionFull}</div>
+        <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+        <div class="divider"></div>
+        <div class="purchase-info">
+          <div class="price">{`£${this.props.item.price.toFixed(2)}`}</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ShopItemClass;
+
+ShopItemClass.propTypes = {
+  item: PropTypes.shape(
+    {
+      brand: PropTypes.string,
+      title: PropTypes.string,
+      description: PropTypes.string,
+      descriptionFull: PropTypes.string,
+      price: PropTypes.number,
+      currency: PropTypes.string
+    }
+  )
+}
